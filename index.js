7a62a401380e1e075e1dec51f24b13beb495212d
[zubair bhuian] <[zubairbhuian@gmail.com]>
2021-12-27 15:00:48 +0600

    pub-heroku-node-complite

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+// const port = process.env.PORT;
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index", {});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
